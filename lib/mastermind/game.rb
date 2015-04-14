140542a32591ddb51ab2da4719d8782f8829d2e7
Sam Han <shan@8thlight.com>
2015-04-14 15:43:06 +0100

    added counting the guesses and indicating the game is over after 10 guesses

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -1,5 +1,7 @@
 module Mastermind
   class Game
+    attr_reader :number_of_guesses
+
     def initialize output
       @output = output
     end
@@ -7,6 +9,16 @@ module Mastermind
     def start
       @output.puts("Welcome to Mastermind!")  
       @output.puts("Start game? Make sure you have a code ready!")
+      @number_of_guesses = 0
+    end
+
+    def guess
+      @number_of_guesses += 1
+      return "R R R R"
+    end
+
+    def over?
+      @number_of_guesses >= 10
     end
   end
 end
--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 module Mastermind
   describe Game do
+    let(:output) { double('output').as_null_object }
+    let(:game) { Game.new(output) }
+    
     describe "#start" do
-      let(:output) { double('output').as_null_object }
-      let(:game) { Game.new(output) }
-
       it "sends a welcome message" do
         expect(output).to receive(:puts).with("Welcome to Mastermind!")
         game.start
@@ -16,6 +16,41 @@ module Mastermind
         game.start
       end
 
+      it "starts with a guess counter at 0" do
+        game.start
+        
+        expect(game.number_of_guesses).to eq(0)
+      end
+    end
+
+    describe "#guess" do
+      it "returns a random guess" do
+        game.start
+        expect(game.guess).to eq("R R R R")
+      end
+
+      it "increments the counter to 1 when the computer makes a guess" do
+        game.start
+        game.guess
+
+        expect(game.number_of_guesses).to eq(1)
+      end
+
+      it "increments the counter each time the computer makes a guess" do
+        game.start
+        game.guess
+        game.guess
+
+        expect(game.number_of_guesses).to eq(2)
+      end
+
+      it "sets the game as over when there are 10 guesses" do
+        game.start
+
+        10.times { game.guess }
+
+        expect(game.over?).to eq(true)
+      end
     end
   end
 end
